5993782c603b68f12efacc9683fb706412a18ceb
Beliyiet <beliyiet@vip.qq.com>
2017-06-02 17:30:49 +0800

    20170601-3

---
+++ app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.fmebicorp.beliyiet.metromastercognac;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by BELIYIET on 2017/6/2.
+ */
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        // 用Handler的postDelayed方法，3秒后执行跳转到MainActivity
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                goHome();
+            }
+        }, 3000);
+    }
+    private void goHome() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
