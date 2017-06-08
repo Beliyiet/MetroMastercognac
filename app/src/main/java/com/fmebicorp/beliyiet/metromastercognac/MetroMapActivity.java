0332d2cd5f77b3660fa1c247d60d258c85ed6abe
Beliyiet <beliyiet@vip.qq.com>
2017-06-08 08:25:33 +0800

    地图基本sdk完成

--- app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
+++ app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
@@ -8,10 +8,18 @@ import com.amap.api.maps.MapView;
 
 
 public class MetroMapActivity extends Activity {
-
+    com.amap.api.maps2d.MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metro_map);
+        //获取地图控件引用
+        mapView = (com.amap.api.maps2d.MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+
     }
+
+
 
 }
