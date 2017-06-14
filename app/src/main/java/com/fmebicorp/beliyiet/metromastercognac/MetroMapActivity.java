804ada5f74edca6ce0977066a84a082b4bc946c5
Beliyiet <beliyiet@vip.qq.com>
2017-06-14 15:45:47 +0800

    地图基本sdk完成,地图读取优化，室内地图功能添加

--- app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
+++ app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
@@ -2,6 +2,11 @@ package com.fmebicorp.beliyiet.metromastercognac;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 
@@ -16,10 +21,31 @@ public class MetroMapActivity extends Activity {
         mapView = (com.amap.api.maps.MapView) findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mapView.onCreate(savedInstanceState);
-        com.amap.api.maps.AMap aMap = mapView.getMap();
+        final com.amap.api.maps.AMap aMap = mapView.getMap();
         aMap.showIndoorMap(true);
 
+        final int[] number = {0};
+        ImageButton mapbutton1 = (ImageButton)findViewById(R.id.map_layers);
+        mapbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (number[0] == 0){
+                    aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 设置卫星地图模式
+                    Toast.makeText(MetroMapActivity.this,"卫星地图模式",Toast.LENGTH_SHORT).show();
+                } else if (number[0] == 1){
+                    aMap.setMapType(AMap.MAP_TYPE_NIGHT);//夜景地图
+                    Toast.makeText(MetroMapActivity.this,"夜景地图模式",Toast.LENGTH_SHORT).show();
+                } else if (number[0] == 2){
+                    aMap.setMapType(AMap.MAP_TYPE_NORMAL);//标准地图
+                    Toast.makeText(MetroMapActivity.this,"标准地图模式",Toast.LENGTH_SHORT).show();
+                }
+                number[0] = (number[0] +1)%3;
+            }
+        });
+        
+
     }
+
 
 
 
