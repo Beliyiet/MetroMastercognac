869e7c5eed5306e90294b3f53775d516eaf25541
Beliyiet <beliyiet@vip.qq.com>
2017-06-16 12:39:29 +0800

    June 14 2017 1.1.alpha_2000     3D地图加入 室内图加入 图标更换 启动界面更换 关于页面修改

--- app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
+++ app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
@@ -3,16 +3,26 @@ package com.fmebicorp.beliyiet.metromastercognac;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.amap.api.maps.AMapOptions;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.MapView;
 
 
-public class MetroMapActivity extends Activity {
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.LatLng;
+
+
+public class MetroMapActivity extends Activity implements LocationSource {
     com.amap.api.maps.MapView mapView;
+    AMap aMap;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,17 @@ public class MetroMapActivity extends Activity {
         mapView.onCreate(savedInstanceState);
         final com.amap.api.maps.AMap aMap = mapView.getMap();
         aMap.showIndoorMap(true);
+
+
+        UiSettings mUiSettings;//定义一个UiSettings对象
+        mUiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+
+
+        //定位中队坐标
+        LatLng centerXJPoint = new LatLng(121.360943,31.226912);
+        AMapOptions mapOptions = new AMapOptions();//
+
+
 
         final int[] number = {0};
         ImageButton mapbutton1 = (ImageButton)findViewById(R.id.map_layers);
@@ -46,9 +67,39 @@ public class MetroMapActivity extends Activity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mapView.onSaveInstanceState(outState);
+    }
 
 
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
 
+    }
 
+    @Override
+    public void deactivate() {
 
+    }
 }
