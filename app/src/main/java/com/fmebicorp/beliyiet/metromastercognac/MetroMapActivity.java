73aff4ff5db8795903b6ae78ab64355edb43027c
Beliyiet <beliyiet@vip.qq.com>
2017-06-29 22:38:30 +0800

    June 29 2017 1.1alpha_2044     暂删蜂鸟云 工具组布局修复 主页布局初步修改

--- app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
+++ app/src/main/java/com/fmebicorp/beliyiet/metromastercognac/MetroMapActivity.java
@@ -2,22 +2,63 @@ package com.fmebicorp.beliyiet.metromastercognac;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.MapView;
 
 
-public class MetroMapActivity extends Activity {
-    com.amap.api.maps2d.MapView mapView;
+import com.amap.api.maps.UiSettings;
+
+
+public class MetroMapActivity extends Activity  {
+    com.amap.api.maps.MapView mapView;
+    AMap aMap;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metro_map);
         //获取地图控件引用
-        mapView = (com.amap.api.maps2d.MapView) findViewById(R.id.map);
+        mapView = (com.amap.api.maps.MapView) findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mapView.onCreate(savedInstanceState);
+        final com.amap.api.maps.AMap aMap = mapView.getMap();
+        aMap.showIndoorMap(true);
 
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mapView.onSaveInstanceState(outState);
     }
 
 
